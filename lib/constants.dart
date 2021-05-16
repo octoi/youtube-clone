4abae32603c4e57528c729cbdb63b8a569f4e66b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-16 08:59:44 +0530

    color codes

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const appPrimaryColor = Color(0xFF0A0A0A);
+const appSecondaryColor = Color(0xFF212121);
+const appPrimaryLight = Color(0xFFFFFFFF);
+const appSecondaryLight = Color(0xFFBFBFBF);
