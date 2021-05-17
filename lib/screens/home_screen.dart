dfbe2a9f754be641a992d2f7f1172be7f7979c49
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-17 10:53:29 +0530

    finally

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/utils/constants.dart';
 import 'package:youtube_clone/widgets/app_bar.dart';
+import 'package:youtube_clone/widgets/stories.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appPrimaryColor,
-      body: CustomScrollView(
-        slivers: [YoutubAppBar()],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55.0),
+        child: YoutubeAppBar(),
+      ),
+      body: ListView(
+        children: [
+          SliverToBoxAdapter(child: Stories()),
+        ],
       ),
     );
   }
