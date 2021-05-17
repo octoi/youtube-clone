1005a98f7d3a1fdf1a2355ca8cb918c94122810c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-17 10:53:40 +0530

    app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/utils/constants.dart';
-import 'package:youtube_clone/widgets/app_bar.dart';
-import 'package:youtube_clone/widgets/stories.dart';
+import 'package:youtube_clone/utils/data.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appPrimaryColor,
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(55.0),
-        child: YoutubeAppBar(),
-      ),
-      body: ListView(
-        children: [
-          SliverToBoxAdapter(child: Stories()),
+      appBar: AppBar(
+        leadingWidth: 100.0,
+        backgroundColor: appSecondaryColor,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 12.0),
+          child: Image.asset("images/logo.png"),
+        ),
+        actions: [
+          IconButton(icon: Icon(Icons.videocam), onPressed: () {}),
+          IconButton(
+              icon: Icon(Icons.search), iconSize: 30.0, onPressed: () {}),
+          IconButton(
+            iconSize: 30.0,
+            icon: CircleAvatar(
+              foregroundImage: NetworkImage(currentUser.profile),
+            ),
+            onPressed: () {},
+          )
         ],
       ),
     );
