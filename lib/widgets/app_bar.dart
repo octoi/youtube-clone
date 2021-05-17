3a581e9bb05d6cdbb6a72ed6d7f1a055ea7e596b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-17 09:51:48 +0530

    app bar

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/utils/constants.dart';
+import 'package:youtube_clone/utils/data.dart';
+
+class YoutubAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      leadingWidth: 100.0,
+      backgroundColor: appSecondaryColor,
+      floating: true,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 12.0),
+        child: Image.asset("images/logo.png"),
+      ),
+      // leading: Text("Hello"),
+      actions: [
+        IconButton(icon: Icon(Icons.cast), onPressed: () {}),
+        IconButton(
+          icon: Icon(Icons.notifications_outlined),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        IconButton(icon: Icon(Icons.search), iconSize: 30.0, onPressed: () {}),
+        IconButton(
+          iconSize: 30.0,
+          icon: CircleAvatar(
+            foregroundImage: NetworkImage(currentUser.profile),
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+}
