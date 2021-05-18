d6228af78874f243d70fa91ad0756a6694a0ac61
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 08:34:23 +0530

    videos

---
+++ lib/widgets/videos.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Videos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.red,
+          height: 100.0,
+          width: double.infinity,
+        ),
+      ],
+    );
+  }
+}
