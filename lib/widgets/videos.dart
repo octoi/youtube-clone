d226c33ca2836a13c4af495340d5f01a67a634a9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 08:38:30 +0530

    changed operator

--- lib/widgets/videos.dart
+++ lib/widgets/videos.dart
@@ -6,7 +6,7 @@ class Videos extends StatelessWidget {
   List<Widget> getVideos() {
     List<Widget> _widgets = [];
 
-    for (int idx = 0; idx > 10; idx++) {
+    for (int idx = 0; idx != 10; idx++) {
       _widgets.add(Video(youtubeVideo: ytVideo));
     }
 
