9696e6f57d0850ba47e50378e20692c03edb8709
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 08:37:13 +0530

    returned video

--- lib/widgets/videos.dart
+++ lib/widgets/videos.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/utils/data.dart';
+import 'package:youtube_clone/widgets/video.dart';
 
 class Videos extends StatelessWidget {
+  List<Widget> getVideos() {
+    List<Widget> _widgets = [];
+
+    for (int idx = 0; idx > 10; idx++) {
+      _widgets.add(Video(youtubeVideo: ytVideo));
+    }
+
+    return _widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.red,
-          height: 100.0,
-          width: double.infinity,
-        ),
-      ],
-    );
+    return Column(children: getVideos());
   }
 }
