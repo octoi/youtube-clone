0ff76f57f7fe0e1ece198ddeb1c04cf0797ba4c8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 09:17:14 +0530

    basic video card

--- lib/widgets/video.dart
+++ lib/widgets/video.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/utils/constants.dart';
 import 'package:youtube_clone/utils/data.dart';
 
 class Video extends StatelessWidget {
@@ -8,6 +9,65 @@ class Video extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      color: appPrimaryColor,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              image: DecorationImage(
+                image: NetworkImage(youtubeVideo.thumbnail),
+                fit: BoxFit.cover,
+              ),
+            ),
+            height: 220.0,
+            child: Stack(
+              children: [],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(youtubeVideo.creator.profile),
+                ),
+                SizedBox(width: 10.0),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            youtubeVideo.title,
+                            style: TextStyle(
+                              color: appPrimaryLight,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          Icon(
+                            Icons.more_vert,
+                            color: appPrimaryLight,
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
