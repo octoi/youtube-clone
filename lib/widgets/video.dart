fbd2cc790c9f187e0a3681cba3ac68bb956288b2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 08:34:19 +0530

    widget for video

---
+++ lib/widgets/video.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/utils/data.dart';
+
+class Video extends StatelessWidget {
+  final YoutubeVideo youtubeVideo;
+
+  Video({this.youtubeVideo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
